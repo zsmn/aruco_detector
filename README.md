f1950278d33386ce86913e83ff2ff624c36548bf
zsmn <zsmn@cin.ufpe.br>
2019-10-26 00:52:09 -0300

    tracker feito com a lib do opencv

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Uma marcação de ArUco é um quadrado composto por uma borda preta e uma matriz
 A borda preta facilita sua rápida detecção e a codificação binária permite que técnicas de correção e ajuste de erro sejam aplicada mais facilmente.
 
 Exemplos de marcações de ArUco
+
 ![alt text](https://docs.opencv.org/trunk/markers.jpg)
 
 Também é visto que uma marcação pode se encontrar rotacionada, mas o processo de detecção precisa ser capaz de determinar sua rotação original.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include<bits/stdc++.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include<opencv2/aruco.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main(){
-    
+
+    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
+    vector<int> markId;
+    vector<vector<Point2f> > markCorner;
+
+    Mat frame_at = imread("test.png");
+
+    aruco::detectMarkers(frame_at, dictionary, markCorner, markId);
+    if(markId.size() > 0){
+        aruco::drawDetectedMarkers(frame_at, markCorner, markId);
+        imshow("Aruco Tracker", frame_at);
+        waitKey();
+    }else{
+        cout << "Couldn't track an aruco mark" << endl;
+    }
+
+    return 0;
 }
